dc715654e8ef235da376330ed2a3c3f09e817e18
ayoub3bidi <mrayoubabidi@gmail.com>
2022-06-27 17:04:39 +0100

    🎉 Project Setup: Apollo server setup

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
-import { MikroORM, RequiredEntityData } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core";
+// import { RequiredEntityData } from "@mikro-orm/core";
+// import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from 'express'
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from 'type-graphql'
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
-    await orm.getMigrator().up();
-    // const post = orm.em.create(Post,{title:"my first post"} as RequiredEntityData<Post> );
-    // await orm.em.persistAndFlush(post);
+    await orm.getMigrator().up()
 
-    const posts = await orm.em.find(Post, {})
-    console.log(posts)
+    const app = express(); 
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    })
+    await apolloServer.start()
+    apolloServer.applyMiddleware({ app })
+
+    app.listen(5000, () => {
+        console.log('server started on localhost: 5000')
+    })
 };
 
 main().catch((err) => {
