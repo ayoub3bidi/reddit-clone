82d68b19ed16c9d3d5e2eb30d26882ef9c51891a
ayoub3bidi <mrayoubabidi@gmail.com>
2022-08-23 12:24:43 +0100

    💄 style: add title & favicon

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -6,6 +6,9 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head />
+        <title>Reddit Clone</title>
+        <link rel="icon" href="https://i.ibb.co/DpT4sQx/logo.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
